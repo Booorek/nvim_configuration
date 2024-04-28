8d4989b2477578aa0d1b6855ec782b73d7092f4f
Booorek <bartosz.sedzikowski31@gmail.com>
2024-04-28 11:26:51 +0200

    keybind

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -6,3 +6,15 @@ vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
 vim.cmd("set number")
 vim.cmd("set relativenumber")
+-- Copy to clipboard
+vim.api.nvim_set_keymap('v', '<leader>y', '"+y', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>Y', '"+yg_', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>y', '"+y', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>yy', '"+yy', { noremap = true })
+
+-- Paste from clipboard
+vim.api.nvim_set_keymap('n', '<leader>p', '"+p', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>P', '"+P', { noremap = true })
+vim.api.nvim_set_keymap('v', '<leader>p', '"+p', { noremap = true })
+vim.api.nvim_set_keymap('v', '<leader>P', '"+P', { noremap = true })
+
